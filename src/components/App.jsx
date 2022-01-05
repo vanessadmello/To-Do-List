6f6ae46d861befc88893512a48be1645f1dbdd29
Vanessa D'mello <dmello.vanessa2001@gmail.com>
2022-01-05 16:36:44 +0530

    final updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-	return;
+	const [newInput, setInputChange] = useState("");
+	const [todoList, setToDoList] = useState([]);
+
+	function handleSubmit() {
+		setToDoList((prevsValue) => {
+			return [...prevsValue, newInput];
+		});
+		setInputChange("");
+	}
+
+	function handleChange(event) {
+		const { value } = event.target;
+		setInputChange(value);
+	}
+
+	return (
+		<div className="container">
+			<div className="heading">
+				<h1>To-Do List</h1>
+			</div>
+			<div className="form">
+				<input type="text" onChange={handleChange} value={newInput} />
+				<button onClick={handleSubmit}>
+					<span>Add</span>
+				</button>
+			</div>
+			<div>
+				<ul>
+					<li>List</li>
+					{todoList.map((item) => {
+						return <li key={item}>{item}</li>;
+					})}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
